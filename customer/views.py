0fad555ea5b8a97b57c7c4fc0f2d3ba9db8771dd
Gokul Raam <gokulraamofficial@gmail.com>
2020-11-11 22:49:52 +0530

    Added a small template file for testing

--- customer/views.py
+++ customer/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hi")
+    return render(request,"customer/index.html")
