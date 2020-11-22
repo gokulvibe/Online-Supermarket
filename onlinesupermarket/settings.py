6dc95c68394787a05aea473c43984ca7fbab914e
Gokul Raam <gokulraamofficial@gmail.com>
2020-11-22 17:04:06 +0530

    Changed allowed hosts

--- onlinesupermarket/settings.py
+++ onlinesupermarket/settings.py
@@ -20,12 +20,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1@l)v)@dvi9$kfdv#tm1r*)omj%$03zn5z4-muyi0*^ldx(2!b'
+#SECRET_KEY = '1@l)v)@dvi9$kfdv#tm1r*)omj%$03zn5z4-muyi0*^ldx(2!b'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '1@l)v)@dvi9$kfdv#tm1r*)omj%$03zn5z4-muyi0*^ldx(2!b')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
+ALLOWED_HOSTS = ['https://sego-shopping.herokuapp.com', 'http://127.0.0.1']
 
 
 # Application definition
